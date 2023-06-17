103d15c71387173374f6cd6c2b4a43a7c3e423c9
Vindi Sovia A <vindisovia@gmail.com>
2023-06-17 22:56:18 +0700

    merapikan tabel ternak

--- resources/views/ternak/index.blade.php
+++ resources/views/ternak/index.blade.php
@@ -29,202 +29,206 @@
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 
+  <!-- =======================================================
+  * Template Name: NiceAdmin - v2.2.2
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
 </head>
 
 <body>
+<!-- ======= Header ======= -->
+<header id="header" class="header fixed-top d-flex align-items-center">
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top d-flex align-items-center">
+<div class="d-flex align-items-center justify-content-between">
+  <a href="dashboard" class="logo d-flex align-items-center">
+    <img src="assets/img/logo.png" alt="">
+    <span class="d-none d-lg-block">SIVENTA</span>
+  </a>
+  <i class="bi bi-list toggle-sidebar-btn"></i>
+</div><!-- End Logo -->
 
-    <div class="d-flex align-items-center justify-content-between">
-      <a href="dashboard" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">SIVENTA</span>
+<nav class="header-nav ms-auto">
+  <ul class="d-flex align-items-center">
+
+    <li class="nav-item d-block d-lg-none">
+      <a class="nav-link nav-icon search-bar-toggle " href="#">
+        <i class="bi bi-search"></i>
       </a>
-      <i class="bi bi-list toggle-sidebar-btn"></i>
-    </div><!-- End Logo -->
-    
-    <nav class="header-nav ms-auto">
-      <ul class="d-flex align-items-center">
+    </li><!-- End Search Icon-->
 
-        <li class="nav-item d-block d-lg-none">
-          <a class="nav-link nav-icon search-bar-toggle " href="#">
-            <i class="bi bi-search"></i>
-          </a>
-        </li><!-- End Search Icon-->
+ <!--Profil-->
+ <li class="nav-item dropdown pe-3">
+    <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+    <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+    <span class="d-none d-md-block dropdown-toggle ps-2">K. nderAson</span>
+    </a>
+    <!-- End Profile Iamge Icon -->
 
-     <!--Profil-->
-     <li class="nav-item dropdown pe-3">
-        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-        <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-        <span class="d-none d-md-block dropdown-toggle ps-2">K. nderAson</span>
+    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+    <li class="dropdown-header">
+        <h6>Kevin Anderson</h6>
+        <span>Web Designer</span>
+    </li>
+    <li>
+        <hr class="dropdown-divider">
+    </li>
+
+    <li>
+        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+        <i class="bi bi-person"></i>
+        <span>My Profile</span>
         </a>
-        <!-- End Profile Iamge Icon -->
+    </li>
+    <li>
+        <hr class="dropdown-divider">
+    </li>
 
-        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-        <li class="dropdown-header">
-            <h6>Kevin Anderson</h6>
-            <span>Web Designer</span>
-        </li>
-        <li>
-            <hr class="dropdown-divider">
-        </li>
-
-        <li>
-            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-            <i class="bi bi-person"></i>
-            <span>My Profile</span>
-            </a>
-        </li>
-        <li>
-            <hr class="dropdown-divider">
-        </li>
-
-        <li>
-            <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-            <i class="bi bi-gear"></i>
-            <span>Account Settings</span>
-            </a>
-        </li>
-        <li>
-            <hr class="dropdown-divider">
-        </li>
-        </ul><!-- End Profile Dropdown Items -->
-        </li><!-- End Profile Nav -->
-        </ul>
-        </nav><!-- End Icons Navigation -->
-
-  </header><!-- End Header -->
-
-  <aside id="sidebar" class="sidebar">
-
-    <ul class="sidebar-nav" id="sidebar-nav">
-
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="dashboard">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
+    <li>
+        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+        <i class="bi bi-gear"></i>
+        <span>Account Settings</span>
         </a>
-      </li><!-- End Dashboard Nav -->
+    </li>
+    <li>
+        <hr class="dropdown-divider">
+    </li>
+    </ul><!-- End Profile Dropdown Items -->
+    </li><!-- End Profile Nav -->
+    </ul>
+    </nav><!-- End Icons Navigation -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-journal-text"></i><span>Kelola Gudang</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="admin">
-              <i class="bi bi-circle"></i><span>Admin</span>
-            </a>
-          </li>
-          <li>
-            <a href="pakan">
-              <i class="bi bi-circle"></i><span>Pakan</span>
-            </a>
-          </li>
-          <li>
-            <a href="ternak">
-              <i class="bi bi-circle"></i><span>Ternak</span>
-            </a>
-          </li>
-          <li>
-            <a href="gudang">
-              <i class="bi bi-circle"></i><span>Gudang</span>
-            </a>
-          </li>
-        </ul>
-      </li><!-- End Forms Nav -->
+</header><!-- End Header -->
 
-      <!--Kelola Gudang-->
-      <li class="nav-item">
-        <a class="nav-link " href="penjualan">
-          <i class="bi bi-journal-text"></i>
-          <span>Kelola Penjualan</span>
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="dashboard">
+      <i class="bi bi-grid"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-journal-text"></i><span>Kelola Gudang</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="admin">
+          <i class="bi bi-circle"></i><span>Admin</span>
         </a>
       </li>
-      <!-- End Kelola Gudang -->
-
-      <!--Laporan-->
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
+      <li>
+        <a href="pakan">
+          <i class="bi bi-circle"></i><span>Pakan</span>
         </a>
-        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-          <a href="laporan">
+      </li>
+      <li>
+        <a href="ternak">
+          <i class="bi bi-circle"></i><span>Ternak</span>
+        </a>
+      </li>
+      <li>
+        <a href="gudang">
+          <i class="bi bi-circle"></i><span>Gudang</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Forms Nav -->
+
+  <!--Kelola Penjualan-->
+  <li class="nav-item">
+    <a class="nav-link " href="penjualan">
+      <i class="bi bi-journal-text"></i>
+      <span>Kelola Penjualan</span>
+    </a>
+  </li>
+  <!-- End Kelola Penjualan -->
+
+  <!--Laporan-->
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-layout-text-window-reverse"></i><span>Laporan</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+      <a href="laporan">
               <i class="bi bi-circle"></i><span>Review Data</span>
             </a>
             <a href="charts">
               <i class="bi bi-circle"></i><span>Diagram</span>
             </a>
-          </li>
-        </ul>
       </li>
-      <!-- End Laporan -->
-
-      <!-- Log Out-->
-      <br><br><br><br><br><br><br><br><br><br><br>
-      <li class= "nav-item">
-        <a class="nav-link " href="login">
-          <i class="bi bi-box-arrow-right"></i>
-            <span>Log Out</span>
-        </a>
-      </li>
-      <!--End Log Out -->
-  </aside><!-- End Sidebar-->
     </ul>
+  </li>
+  <!-- End Laporan -->
 
-  <main id="main" class="main">
+  <!-- Log Out-->
+  <br><br><br><br><br><br><br><br><br><br><br>
+  <li class= "nav-item">
+    <a class="nav-link " href="login">
+      <i class="bi bi-box-arrow-right"></i>
+        <span>Log Out</span>
+    </a>
+  </li>
+  <!--End Log Out -->
+</aside><!-- End Sidebar-->
+</ul>
 
-    <div class="pagetitle">
-      <h1>Daftar Data Ternak</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="tabel-ternak">Kelola Gudang</a></li>
-          <li class="breadcrumb-item">Daftar Data Ternak</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+<main id="main" class="main">
 
-    <div class="row mb-3">
-        <div class="col-sm-6">
-            <a href ="{{ url('ternak/create') }}">
-            <button type="submit" class="btn btn-primary">Tambah Ternak</button>
-            <a href="{{ url('cetak-ternak') }}" target="_blank" class="btn btn-primary">Cetak Data <i class="fa-solid fa-print"></i></a>
-          </div>
+<div class="pagetitle">
+  <h1>Daftar Data Ternak</h1>
+  <nav>
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="tabel-admin">Kelola Ternak</a></li>
+      <li class="breadcrumb-item">Daftar Data Ternak</li>
+    </ol>
+  </nav>
+</div><!-- End Page Title -->
+
+<div class="row mb-3">
+        <div class="col-sm-6">    
+      <a class="btn btn-primary" button type="submit" href="{{url('ternak/create') }}">Tambah Ternak</a>
+      <a href="{{ url('cetak-ternak') }}" target="_blank" class="btn btn-primary">Cetak Data <i class="fa-solid fa-print"></i></a>
     </div>
+</div>
 
-    <section class="section">
-      <div class="row">
-        <div class="col-lg-12">
+<section class="section">
+  <div class="row">
+    <div class="col-lg-12">
 
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Daftar Ternak</h5>
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Daftar Ternak</h5>
 
-              <!-- Table with stripped rows -->
-              <table class="table datatable">
-                <thead>
-                  <tr>
-                    <th scope="col">ID Ternak</th>
-                    <th scope="col">ID Pakan</th>
-                    <th scope="col">ID Admin</th>
-                    <th scope="col">Jumlah</th>
-                    <th scope="col">Jenis Ternak</th>
-                    <th scope="col">Tanggal Masuk</th>
-                    <th scope="col">Tanggal Keluar</th>
-                  </tr>
+          <!-- Table with stripped rows -->
+          <table class="table datatable">
+            <thead>
+              <tr>
+                <th scope="col">ID Ternak</th>
+                <th scope="col">ID Pakan</th>
+                <th scope="col">ID Admin</th>
+                <th scope="col">Jumlah</th>
+                <th scope="col">Jenis Ternak</th>
+                <th scope="col">Tanggal Masuk</th>
+                <th scope="col">Tanggal Keluar</th>
+              </tr>
 
-                  @foreach ($ternak as $key=>$item)
-                   <tr>
-                    <td>{{$item->id_ternak}}<td>
-                    <td>{{$item->id_pakan}}<td>
-                    <td>{{$item->id_admin}}<td>
-                     <td>{{$item->jumlah}}</td>
-                     <td>{{$item->jenis_ternak}}</td>
-                     <td>{{$item->tanggal_masuk}}</td>
-                     <td>{{$item->tanggal_keluar}}</td>
-                     <td>
+              @foreach ($ternak as $key => $item)
+              <tr>
+                <td>{{$item->id_ternak}}</td>
+                <td>{{$item->id_pakan}}</td>
+                <td>{{$item->id_admin}}</td>
+                <td>{{$item->jumlah}}</td>
+                <td>{{$item->jenis_ternak}}</td>
+                <td>{{$item->tanggal_masuk}}</td>
+                <td>{{$item->tanggal_keluar}}</td>
+                <td>
                       <a href ="{{ url('ternak/'.$item->id_ternak.'/edit') }}">
                       <button type="submit" class="btn btn-primary">EDIT</button></a>
                     </td>
@@ -236,23 +240,23 @@
                       <button class="btn btn-danger" type="submit">HAPUS</button></input>
                       </form>
                     </td> 
-                     @endforeach
-                    </tr>
-
-                </thead>
-              </table>
-              <!-- End Table with stripped rows -->
-            </div>
-          </div>
-
+                @endforeach
+              <tr>
+            </thead>
+          </table>
+          <!-- End Table with stripped rows -->
         </div>
       </div>
-    </section>
 
-  </main><!-- End #main -->
+    </div>
+  </div>
+</section>
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
+</main><!-- End #main -->
+
+
+   <!-- ======= Footer ======= -->
+   <footer id="footer" class="footer">
     <div class="copyright">
       &copy; Copyright <strong><span>SIVENTA</span></strong>. All Rights Reserved
     </div>
